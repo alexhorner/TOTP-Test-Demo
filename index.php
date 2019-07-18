0cac777695565d7d5770515c72e1d9cb0602a262
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2019-07-19 00:46:35 +0300

    Main code built and tested, added QR libraries to composer.json

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    require($_SERVER["DOCUMENT_ROOT"].'/vendor/autoload.php'); //Composer autoload for dependencies
+
+    use OTPHP\TOTP;
+    use OTPHP\Factory;
+
+    use Endroid\QrCode\ErrorCorrectionLevel;
+    use Endroid\QrCode\LabelAlignment;
+    use Endroid\QrCode\QrCode;
+    use Endroid\QrCode\Response\QrCodeResponse;
+
+    if(isset($_GET["uri"]) && $_GET["uri"] != ""){
+        $otp = Factory::loadFromProvisioningUri($_GET["uri"]);        
+    }else{
+        $otp = TOTP::create(null, 30);
+        $otp->setLabel('demo@example.com');
+        header("Location: /?uri=".urlencode($otp->getProvisioningUri()));
+        die();
+    }
+
+    //URI QR Code
+    $qrCode = new QrCode($otp->getProvisioningUri());
+    $qrCode->setSize(300);
+    $qrCodeEncoded = base64_encode($qrCode->writeString());
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            * {
+                font-family: sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>
+            <?php
+                $otpNow = $otp->now();
+                print(substr($otpNow, 0, 3)." ".substr($otpNow, 3, 6));
+            ?>
+        </h1>
+        <br />
+        <?php print('The OTP URI is <input type="text" value="'.$otp->getProvisioningUri().'" /> and should be copied and saved if you want to reproduce this key'); ?><br /><br />
+        The below QR code can be scanned with Google Authenticator to add it to the list. Afterwards, this page and Google Authenticator should display the same numbers<br />
+        <img src="data:image/png;base64,<?php print($qrCodeEncoded); ?>" />
+    </body>
+</html>
